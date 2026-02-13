58cfefc752c4ac0fabaad19be94651dc2bbf420b
SusilKarthik-17 <238w1a4261@vrsec.ac.in>
2026-02-13 19:59:49 +0530

    written code in parser.py

--- backend/config/dask_config.py
+++ backend/config/dask_config.py
@@ -3,14 +3,17 @@ from dask.distributed import Client, LocalCluster
 #client (): client is a python method which is used to make a connection between dask cluster and
 
 #  python code which is mean as local machine 
-# local cluster : it is a cluster used to develop and test or prove distributed computing locally before 
-# deploying into main cluster
-# without client, 
-# we cannot know no.of workers exist in clusters
-# we cannot know what is the task or where the task is running and cannot know status of dask 
-# task : we cannot monitor the process of execution 
-# with client: we can assign task to workers and can easily connect python code with dask and 
-# can see the status of dask
+# local cluster : it is a cluster used to develop and test or prove distributed computing locally before deploying into main cluster
+
+'''
+without client, 
+we cannot know no.of workers exist in clusters
+we cannot know what is the task or where the task is running and cannot know status of dask 
+task : we cannot monitor the process of execution 
+with client: we can assign task to workers and can easily connect python code with dask and 
+can see the status of dask
+
+'''
 
 def create_dask_client():
     cluster = LocalCluster(
--- backend/schema/schema.py
+++ backend/schema/schema.py
@@ -1,16 +1,14 @@
-'''
---> schema files define the structure of the dataand type of the data.
 
---> Schema is nothing but role book of our data.
+# --> schema files define the structure of the data and type of the data.
 
-Uses of Schema Files:
-1. It ensures all the rows follows the same data structure and type.
-2. It helps in data validation and data cleaning(correct the data type)
-3. Faster Processing, easier analytics of data files is possible 
-(faster processing because it knows the data type and structure.)
-(easy analysis means grouping, mapping, filtering, anamoly detection becomes simple and faster.)
+# --> Schema is nothing but role book of our data.
 
-'''
+# Uses of Schema Files:
+# 1. It ensures all the rows follows the same data structure and type.
+# 2. It helps in data validation and data cleaning(correct the data type)
+# 3. Faster Processing, easier analytics of data files is possible 
+# (faster processing because it knows the data type and structure.)
+# (easy analysis means grouping, mapping, filtering, anamoly detection becomes simple and faster.)
 
 log_schema = {
     "timestamp" : "datetime",
